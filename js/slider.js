eced1defe0c59dc23bbcae3a808b0f1fa19d237e
André Luiz Pastore Dambros <andreluizpd@gmail.com>
2019-01-14 18:37:38 +0000

    Added all images

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,16 @@
-var bgImageArray = ["background-one", "background-two", "background-three", "background-four"],
+var bgImageArray = ["background-one", "background-two", "background-three", 
+	"background-four", "background-five", "background-six", "background-seven", 
+	"background-eight", "background-nine", "background-ten"],
+
 base = "hero is-light is-fullheight ",
+
 secs = 6;
 
-var bgLinksArray = ["01.JPG", "02.JPG", "03.JPG"],
+var bgLinksArray = ["01.JPG", "02.JPG", "03.JPG", "04.JPG", "05.JPG", 
+	"06.JPG", "07.JPG", "08.JPG", "09.JPG", "10.JPG"],
+
 link = "../img/";
+
 bgLinksArray.forEach(function(img){
     new Image().src = link + img; 
     // caches images, avoiding white flash between background replacements
@@ -18,7 +25,8 @@ function backgroundSequence() {
 
 			document.getElementById('background').className = base + bgImageArray[k];
 
-		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
+		if ((k + 1) === bgImageArray.length) { 
+			setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
 		}, (secs * 1000) * i)	
 	}
 }
